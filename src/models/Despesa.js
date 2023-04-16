cc401dcf06ece109bb7c0bee30f7c61c08174351
edipojoseoliveira <edipojoseoliveira@gmail.com>
2023-04-16 00:03:24 -0300

    Liberacao do cors

--- src/models/Despesa.js
+++ src/models/Despesa.js
@@ -5,7 +5,7 @@ const despesaSchema = new mongoose.Schema(
         id: {type: String},
         descricao: {type: String, required: true},
         valor: {type: Number, required: true},
-        grupo: {type: mongoose.Schema.Types.ObjectId, ref: 'grupos', required: true},
+        grupo: {type: mongoose.Schema.Types.ObjectId, ref: 'grupos', required: false},
     }
 );
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from 'cors';
 import DespesaRouter from "./despesasRoutes.js";
 import GrupoRouter from "./gruposRoutes.js";
 
 const routes = (app) => {
+    app.use(cors());
+    
     app.route('/').get((req, res) => {
         res.status(200).send({titulo: 'Selfie Financeira'})
     });
