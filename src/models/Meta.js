b414422b74e243ba22f5abcca46319d3f189a040
EdiCursos <edicursosprofessor@gmail.com>
2023-05-01 22:26:20 -0300

    Campo para guardar o valor poupado da meta

--- src/models/Meta.js
+++ src/models/Meta.js
@@ -4,6 +4,7 @@ const metaSchema = new mongoose.Schema(
     {
         id: {type: String},
         valor: {type: Number, required: true},
+        valorGuardado: {type: Number, required: true},
         data: {type: Date, required: true},
         nome: {type: String, required: true},
         motivo: {type: String, required: true},
