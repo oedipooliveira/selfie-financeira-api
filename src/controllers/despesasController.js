d1dc0b825069aca7e2cfcba4a37bba2481221e77
EdiCursos <edicursosprofessor@gmail.com>
2023-05-14 23:00:43 -0300

    Filtros por mes

--- src/controllers/despesasController.js
+++ src/controllers/despesasController.js
@@ -3,11 +3,56 @@ import despesas from "../models/Despesa.js";
 class DespesaController {
 
     static listarDespesas = (req, res) => {
-        despesas.find().populate('grupo').exec().then(result => {
-            res.status(200).json(result);
-        }).catch(err => {
-            console.error(err);
-        });
+        // String com o mês e ano (exemplo: "Maio/2023")
+        const mesAnoString = req.query.periodo;
+
+        // Obter o mês e o ano da string
+        const partes = mesAnoString.split("/");
+        const mesString = partes[0]; // "Maio"
+        const anoString = partes[1]; // "2023"
+
+        // Mapear o nome do mês para um número
+        const meses = {
+            "Janeiro": 1,
+            "Fevereiro": 2,
+            "Março": 3,
+            "Abril": 4,
+            "Maio": 5,
+            "Junho": 6,
+            "Julho": 7,
+            "Agosto": 8,
+            "Setembro": 9,
+            "Outubro": 10,
+            "Novembro": 11,
+            "Dezembro": 12
+        };
+
+        // Converter o nome do mês para um número
+        const mesNumero = meses[mesString];
+
+        // Verificar se o mês é válido
+        if (!mesNumero) {
+          mesNumero = 1;
+        }
+
+        // Converter o ano e mês para um objeto Date
+        const primeiroDiaDoMes = new Date(anoString, mesNumero - 1, 0);
+        const ultimoDiaDoMes = new Date(anoString, mesNumero, 0);
+
+        // Consulta de agregação para buscar as despesas do mês
+        despesas
+            .find({
+                vencimento: {
+                    $gte: primeiroDiaDoMes,
+                    $lte: ultimoDiaDoMes
+                }
+            })
+            .populate('grupo').exec()
+            .then(result => {
+                res.status(200).json(result);
+            }).catch(err => {
+                console.error(err);
+            });
     }
 
     static listarDespesaPorId = (req, res) => {
@@ -58,7 +103,52 @@ class DespesaController {
     }
 
      static totalDespesas = (req, res) => {
+        // String com o mês e ano (exemplo: "Maio/2023")
+        const mesAnoString = req.query.periodo;
+
+        // Obter o mês e o ano da string
+        const partes = mesAnoString.split("/");
+        const mesString = partes[0]; // "Maio"
+        const anoString = partes[1]; // "2023"
+
+        // Mapear o nome do mês para um número
+        const meses = {
+            "Janeiro": 1,
+            "Fevereiro": 2,
+            "Março": 3,
+            "Abril": 4,
+            "Maio": 5,
+            "Junho": 6,
+            "Julho": 7,
+            "Agosto": 8,
+            "Setembro": 9,
+            "Outubro": 10,
+            "Novembro": 11,
+            "Dezembro": 12
+        };
+
+        // Converter o nome do mês para um número
+        const mesNumero = meses[mesString];
+
+        // Verificar se o mês é válido
+        if (!mesNumero) {
+          mesNumero = 1;
+        }
+
+        // Converter o ano e mês para um objeto Date
+        const primeiroDiaDoMes = new Date(anoString, mesNumero - 1, 0);
+        const ultimoDiaDoMes = new Date(anoString, mesNumero, 0);
+
+        // Consulta de agregação para buscar as despesas do mês
         despesas.aggregate([
+            {
+                $match: {
+                    vencimento: {
+                        $gte: primeiroDiaDoMes,
+                        $lte: ultimoDiaDoMes
+                    }
+                }
+            },
             {
                 $group: {
                     _id: null,
@@ -66,7 +156,7 @@ class DespesaController {
                 }
             }
         ]).then(result => {
-            const totalDespesas = result[0].total;
+            const totalDespesas = result.length > 0 ? result[0].total : 0;
             res.status(200).json({ totalDespesas });
         }).catch(err => {
             res.status(500).send({message: `${err.message} - falha ao calcular o total das despesas.`});
--- src/controllers/receitasController.js
+++ src/controllers/receitasController.js
@@ -3,11 +3,57 @@ import receitas from "../models/Receita.js";
 class ReceitaController {
 
     static listarReceitas = (req, res) => {
-        receitas.find().populate('grupo').exec().then(result => {
-            res.status(200).json(result);
-        }).catch(err => {
-            console.error(err);
-        });
+        // String com o mês e ano (exemplo: "Maio/2023")
+        const mesAnoString = req.query.periodo;
+
+        // Obter o mês e o ano da string
+        const partes = mesAnoString.split("/");
+        const mesString = partes[0]; // "Maio"
+        const anoString = partes[1]; // "2023"
+
+        // Mapear o nome do mês para um número
+        const meses = {
+            "Janeiro": 1,
+            "Fevereiro": 2,
+            "Março": 3,
+            "Abril": 4,
+            "Maio": 5,
+            "Junho": 6,
+            "Julho": 7,
+            "Agosto": 8,
+            "Setembro": 9,
+            "Outubro": 10,
+            "Novembro": 11,
+            "Dezembro": 12
+        };
+
+        // Converter o nome do mês para um número
+        const mesNumero = meses[mesString];
+
+        // Verificar se o mês é válido
+        if (!mesNumero) {
+          mesNumero = 1;
+        }
+
+        // Converter o ano e mês para um objeto Date
+        const primeiroDiaDoMes = new Date(anoString, mesNumero - 1, 0);
+        const ultimoDiaDoMes = new Date(anoString, mesNumero, 0);
+
+        // Consulta de agregação para buscar as despesas do mês
+        receitas
+            .find({
+                recebimento: {
+                    $gte: primeiroDiaDoMes.toISOString(),
+                    $lte: ultimoDiaDoMes.toISOString()
+                }
+            })
+            .populate('grupo').exec()
+            .then(result => {
+                console.log(result);
+                res.status(200).json(result);
+            }).catch(err => {
+                console.error(err);
+            });
     }
 
     static listarReceitaPorId = (req, res) => {
@@ -58,7 +104,52 @@ class ReceitaController {
     }
 
     static totalReceitas = (req, res) => {
+        // String com o mês e ano (exemplo: "Maio/2023")
+        const mesAnoString = req.query.periodo;
+
+        // Obter o mês e o ano da string
+        const partes = mesAnoString.split("/");
+        const mesString = partes[0]; // "Maio"
+        const anoString = partes[1]; // "2023"
+
+        // Mapear o nome do mês para um número
+        const meses = {
+            "Janeiro": 1,
+            "Fevereiro": 2,
+            "Março": 3,
+            "Abril": 4,
+            "Maio": 5,
+            "Junho": 6,
+            "Julho": 7,
+            "Agosto": 8,
+            "Setembro": 9,
+            "Outubro": 10,
+            "Novembro": 11,
+            "Dezembro": 12
+        };
+
+        // Converter o nome do mês para um número
+        const mesNumero = meses[mesString];
+
+        // Verificar se o mês é válido
+        if (!mesNumero) {
+          mesNumero = 1;
+        }
+
+        // Converter o ano e mês para um objeto Date
+        const primeiroDiaDoMes = new Date(anoString, mesNumero - 1, 0);
+        const ultimoDiaDoMes = new Date(anoString, mesNumero, 0);
+
+        // Consulta de agregação para buscar as despesas do mês
         receitas.aggregate([
+            {
+                $match: {
+                    recebimento: {
+                        $gte: primeiroDiaDoMes,
+                        $lte: ultimoDiaDoMes
+                    }
+                }
+            },
             {
                 $group: {
                     _id: null,
@@ -66,7 +157,7 @@ class ReceitaController {
                 }
             }
         ]).then(result => {
-            const totalReceitas = result[0].total;
+            const totalReceitas = result.length > 0 ? result[0].total : 0;
             res.status(200).json({ totalReceitas });
         }).catch(err => {
             res.status(500).send({message: `${err.message} - falha ao calcular o total das receitas.`});
