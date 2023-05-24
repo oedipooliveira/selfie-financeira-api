84476f92ebad061dc3c64946a3f1d15e5b205f6f
edipojoseoliveira <edipojoseoliveira@gmail.com>
2023-05-23 21:04:36 -0300

    Rota de usuários

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -2,23 +2,6 @@ import usuarios from "../models/Usuario.js";
 
 class UsuarioController {
 
-    static listarUsuarios = (req, res) => {
-        usuarios.find().then(result => {
-            res.status(200).json(result);
-        }).catch(err => {
-            console.error(err);
-        });
-    }
-
-    static listarUsuarioPorId = (req, res) => {
-        const id = req.params.id;
-        usuarios.findById(id).then(result => {
-            res.status(200).send(result);
-        }).catch(err => {
-            res.status(400).send({message: `${err.message} - Id do usuário não localizado.`})
-        });
-    }
-
     static cadastrarUsuario = (req, res) => {
         let usuario = new usuarios(req.body);
         usuario.save().then(result => {
@@ -26,26 +9,6 @@ class UsuarioController {
         }).catch(err => {
             res.status(500).send({message: `${err.message} - falha ao salvar o usuário.`})
         })
-    }
-
-    static atualizarUsuario = (req, res) => {
-        const id = req.params.id;
-
-        usuarios.findByIdAndUpdate(id, {$set: req.body}).then(result => {
-            res.status(200).send({message: 'Usuário atualizado com sucesso.'});
-        }).catch(err => {
-            res.status(500).send({message: `${err.message} - falha ao atualizar o usuário.`})
-        });
-    }
-
-    static excluirUsuario = (req, res) => {
-        const id = req.params.id;
-
-        usuarios.findByIdAndDelete(id).then(result => {
-            res.status(200).send({message: 'Usuário removido com sucesso.'})
-        }).catch(err => {
-            res.status(500).send({message: `${err.message} - falha ao excluir o usuário.`})
-        });
     }
 
 }
--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -4,10 +4,6 @@ import UsuarioController from "../controllers/usuariosController.js";
 const UsuarioRouter = express.Router();
 
 UsuarioRouter
-    .get("/usuarios", UsuarioController.listarUsuarios)
-    .get("/usuarios/:id", UsuarioController.listarUsuarioPorId)
     .post("/usuarios", UsuarioController.cadastrarUsuario)
-    .put("/usuarios/:id", UsuarioController.atualizarUsuario)
-    .delete("/usuarios/:id", UsuarioController.excluirUsuario)
 
 export default UsuarioRouter;
